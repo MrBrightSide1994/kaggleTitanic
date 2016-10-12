91611c6d79eaedb8ea6b1513942828a742b794da
alexbzmn <crashius@gmail.com>
2016-10-12 16:27:41 +0200

    refactoring

--- jakesherman.py
+++ jakesherman.py
@@ -66,6 +66,7 @@ def create_dummy_nans(data, col_name):
 def impute(data):
     """Impute missing values in the Age, Deck, Embarked, and Fare features.
     """
+
     impute_missing = data.drop(['Survived', 'Train'], axis=1)
     impute_missing_cols = list(impute_missing)
     filled_soft = fancyimpute.MICE().complete(np.array(impute_missing))
@@ -73,29 +74,30 @@ def impute(data):
     results['Train'] = list(data['Train'])
     results['Survived'] = list(data['Survived'])
     assert results.isnull().sum().sum() == 0, 'Not all NAs removed'
+
     return results
 
 
 def feature_engineering(data):
-    return (data
-
+    data = (data
             # Create last name, title, family size, and family features
             .assign(LastName=lambda x: x.Name.map(extract_lastname))
             .assign(Title=lambda x: x.Name.map(extract_title))
             .assign(FamSize=lambda x: x.SibSp + x.Parch + 1)
             .assign(Family=lambda x: [a + '_' + str(b) for a, b in zip(
         list(x.LastName), list(x.FamSize))])
-
-            # Create ticket counts for passengers sharing tickets
-            .pipe(ticket_counts)
-
             # Turn the Cabin feature into a Deck feature (A-G)
             .assign(Deck=lambda x: np.where(
         pd.notnull(x.Cabin), first_letter(x.Cabin.fillna('z')), x.Cabin))
             .assign(Deck=lambda x: np.where(x.Deck == 'T', np.nan, x.Deck))
 
             # Turn Sex into a dummy variable
-            .assign(Sex=lambda x: np.where(x.Sex == 'male', 1, 0))
+            .assign(Sex=lambda x: np.where(x.Sex == 'male', 1, 0)))
+
+    return (data
+
+            # Create ticket counts for passengers sharing tickets
+            .pipe(ticket_counts)
 
             # Create dummy variables for the categorical features
             .assign(Pclass=lambda x: x.Pclass.astype(str))
@@ -110,7 +112,7 @@ def feature_engineering(data):
                   axis=1)
 
             # Impute NAs using MICE
-            .pipe(impute)
+            # .pipe(impute)
             )
 
 
@@ -119,11 +121,13 @@ def split_data(data):
     Split the combined training/prediction data into separate training and
     prediction sets.
     """
+
     outcomes = np.array(data.query('Train == 1')['Survived'])
     train = (data.query('Train == 1')
              .drop(['Train', 'Survived'], axis=1))
     to_predict = (data.query('Train == 0')
                   .drop(['Train', 'Survived'], axis=1))
+
     return train, outcomes, to_predict
 
 
@@ -134,12 +138,13 @@ def train_test_model(model, hyperparameters, X_train, X_test, y_train, y_test,
     matricies corresponding to hold-out cross-validation, returns a model w/
     optimized hyperparameters, and prints out model evaluation metrics.
     """
+
     optimized_model = GridSearchCV(model, hyperparameters, cv=folds,
                                    n_jobs=-1)
     optimized_model.fit(X_train, y_train)
-    predicted = optimized_model.predict(X_test)
-    print('Optimized parameters:', optimized_model.best_params_)
+    optimized_model.predict(X_test)
 
+    print('Optimized parameters:', optimized_model.best_params_)
     print('Model accuracy (hold-out):', optimized_model.score(X_test, y_test))
 
     kfold_score = np.mean(cross_val_score(
@@ -155,76 +160,79 @@ def majority_vote_ensemble(name, models_votes, train, outcomes, to_predict):
     """Creates a submission from a majority voting ensemble, given training/
     testing data and a list of models and votes.
     """
+
     model_results = []
     for model, votes in models_votes:
         model.fit(np.array(train), outcomes)
         probs = model.predict(np.array(to_predict))
         probs[probs == 0] = -1
         model_results.append((probs, votes))
+
     ensemble = pd.read_csv('data/test.csv')[['PassengerId']].assign(
         Survived=0)
+
     for probs, votes in model_results:
         for i in range(0, votes):
             ensemble = ensemble.assign(Survived=lambda x: x.Survived + probs)
+
     (ensemble.assign(Survived=lambda x: np.where(x.Survived > 0, 1, 0))
      .to_csv(name, index=False))
+
     return None
 
 
-def model_and_submit(train, outcomes, to_predict, name, find_hyperparameters):
+def model_and_submit(train, outcomes, to_predict, output_file_name, find_hyperparameters):
     """
     Use a random forest classifier to predict which passengers survive the
     sinking of the Titanic and create a submission.
     """
+
     if find_hyperparameters:
         X_train, X_test, y_train, y_test = train_test_split(
             train, outcomes, test_size=0.2, random_state=50)
+
         rf_model = train_test_model(
             RandomForestClassifier(n_estimators=800, random_state=25), {
                 'min_samples_split': [1, 3, 10],
                 'min_samples_leaf': [1, 3, 10],
                 'max_depth': [3, None]},
             X_train, X_test, y_train, y_test).best_estimator_
+
         lr_model = train_test_model(
             LogisticRegression(random_state=25), {
                 'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
                 'class_weight': [None, 'balanced']},
             X_train, X_test, y_train, y_test).best_estimator_
+
         svm_model = train_test_model(
             SVC(probability=True, random_state=25), {
                 'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
                 'gamma': np.logspace(-9, 3, 13)},
             X_train, X_test, y_train, y_test).best_estimator_
-        # gbt_model = train_test_model(
-        #     xgb.XGBClassifier(learning_rate=0.05, n_estimators=200,
-        #                       seed=25), {
-        #         'max_depth': range(3, 10, 2),
-        #         'min_child_weight': range(1, 6, 2),
-        #         'gamma': [i / 10.0 for i in range(0, 5)],
-        #         'reg_alpha': [0.001, 0.01, 0.1, 1, 10, 100, 1000]},
-        #     np.array(X_train), np.array(X_test), y_train,
-        #     y_test).best_estimator_
+
     else:
         rf_model = RandomForestClassifier(n_estimators=800, random_state=25,
                                           min_samples_split=3, max_depth=None, min_samples_leaf=1)
+
         lr_model = LogisticRegression(random_state=25, C=10,
                                       class_weight='balanced')
+
         svm_model = SVC(probability=True, random_state=25, C=1000,
                         gamma=0.0001)
-        # gbt_model = xgb.XGBClassifier(learning_rate=0.05, n_estimators=200,
-        #                               seed=25, reg_alpha=0.01, max_depth=3, gamma=0.1,
-        #                               min_child_weight=1)
+
     models_votes = [(rf_model, 2), (lr_model, 1), (svm_model, 1)]
-    majority_vote_ensemble(name, models_votes, train, outcomes, to_predict)
+    majority_vote_ensemble(output_file_name, models_votes, train, outcomes, to_predict)
     return None
 
 
 def main():
     data = ingest_data()
     data = feature_engineering(data)
-    train, outcomes, to_predict = split_data(data)
-    model_and_submit(train, outcomes, to_predict, name='Output_titanic',
-                     find_hyperparameters=True)
+
+    print(data)
+    # train, outcomes, to_predict = split_data(data)
+    # model_and_submit(train, outcomes, to_predict, name='Output_titanic',
+    #                  find_hyperparameters=True)
 
 
 if __name__ == '__main__':
